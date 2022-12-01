d92f36512e8d6bbc4dfcc1534563edcb553d2af6
AlexGoussatchok <Alex.Goussatchok@gmail.com>
2022-12-01 21:55:13 +0300

    Initial commit

---
+++ model/calculator.py
@@ -0,0 +1,23 @@
+class Calculator:
+
+    @staticmethod
+    def sum(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        return a // b
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
